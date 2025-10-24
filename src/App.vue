380a719f2fdeb65fe2e0cd4a7743dca8986259d3
Marius Rusnak <mariusrusnak@gmail.com>
2025-10-24 11:40:09 +0300

    add tailwindCss

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,16 @@ const clickHandler = () => {
 const changeHandler = () => console.log('You changed me!')
 const inputHandler = (e) => console.log('You typed: ' + e.target.value)
 const keydownHandler = (e) => console.log('You pressed: ' + e.key)
+const link = ref('https://emanuel.ro')
+const randomNumber = ref(null)
+const counter = ref(0)
+const incrementCounter = () => {
+  counter.value++
+}
+
+const updateRandomNumber = () => {
+  randomNumber.value = Math.random()
+}
 
 onMounted(() => console.log('App mounted'))
 onUpdated(() => console.log('App updated'))
@@ -19,21 +29,54 @@ onUpdated(() => console.log('App updated'))
   <h1>{{ message }}</h1>
 
   <button @click="clickHandler">Click me</button>
+
+  <p>{{ message }}</p>
+
   <br />
-  <p>
-    <input 
-      @change="changeHandler" 
-      @input="inputHandler($event)" 
-      @keydown="keydownHandler($event)"
-      type="text" 
-    />
-  </p>
+
+  <input type="text" v-model="message" @change="changeHandler" @input="inputHandler($event)"
+    @keydown="keydownHandler($event)" />
+
+  <br /><br />
+
+  <a :href="link" target="_blank">Visit Emanuel</a>
+
+  <hr />
+
+  <h3>Display a random number</h3>
+  <button @click="updateRandomNumber">Get a random number{{randomNumber}}</button>
+
+  <div v-if="randomNumber">
+    <div v-if="randomNumber >= 0.5">
+      The random number is greater than 0.5
+    </div>
+    <div v-else>
+      The random number is less than  0.5
+    </div>
+  </div>
+  <br>
+   <div v-if="randomNumber && randomNumber >= 0.5">
+      The random number is greater than 0.5
+    </div>
+    <div v-else-if="randomNumber">
+      The random number is less than  0.5
+    </div>
+
+    <br>
+    <hr>
+    <h3> Counter </h3>
+    <button @click="counter++">Increment</button>
+    <div>{{counter}}</div>
 </template>
 
-<style scoped>
+
+<style>
+
+@import "tailwindcss";
+</style>
 /* #app {
   text-align: left;
   margin-top: 4rem;
   color: rgb(212, 24, 143);
 } */
-</style>
+
