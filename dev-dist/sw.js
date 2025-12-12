e658c7a077603134195f5099440b30f5049105b4
Marius Rusnak <mariusrusnak@gmail.com>
2025-12-12 11:25:18 +0200

    checkApp

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rkn39e62kh"
+    "revision": "0.0i5q3itu49g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.js
+++ vite.config.js
@@ -33,15 +33,27 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "icons/favicon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
             purpose: "any",
           },
           {
-            src: "icons/favicon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
             purpose: "maskable",
           },
         ],
