4ae0167e15b199c70879bab38fd4db20d4c71b96
Marius Rusnak <mariusrusnak@gmail.com>
2025-12-12 11:34:48 +0200

    check

--- vite.config.js
+++ vite.config.js
@@ -16,49 +16,19 @@ export default defineConfig({
     vueDevTools(),
     VitePWA({
       registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
       devOptions: {
         enabled: true,
       },
       manifest: {
-        name: "Curs4 TAW",
-        short_name: "Curs4",
-        description: "Aplicație Vue cu PWA - Curs Tehnici și Aplicații Web",
         theme_color: "#169bcb",
-        background_color: "#ffffff",
-        display: "standalone",
-        scope: "/",
-        start_url: "/",
         icons: [
           {
-            src: "icons/icon-192x192.png",
+            src: "icons/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
           },
         ],
       },
-      screenshots: [],
     }),
   ],
   resolve: {
